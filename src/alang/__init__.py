ebad3942d0dbfb07821a7a8460d2807a7e0cfdbd
Frank A. Krueger <fak@praeclarum.org>
2024-10-10 12:02:39 -0700

    Rework nodes

--- src/alang/__init__.py
+++ src/alang/__init__.py
@@ -1,12 +1,12 @@
 from nodes import NodeType, TypeRef, Function
 
 def define(name: str, *parameters: list[tuple[str, TypeRef]]) -> Function:
-    n = Function()
-    n.set_name(name)
+    n = Function(name)
     for param in parameters:
         n.parameter(param)
     return n
 
 if __name__ == "__main__":
     print(define("f", "x"))
+    pass
 
--- src/alang/nodes.py
+++ src/alang/nodes.py
@@ -1,79 +1,135 @@
 
+import io
 from typing import Callable, Optional, TypeVar, Union
 
 TypeRef = str
 
 class NodeType:
     FUNCTION = 'function'
+    PARAMETER = 'parameter'
+    TYPE = 'type'
 
 next_node_id = 1
 
-class NodeData:
-    def __init__(self, type: NodeType, node: Optional["Node"] = None):
+class Node:
+    def __init__(self, type: NodeType):
         global next_node_id
         self.id = next_node_id
         next_node_id += 1
         self.type = type
-        self.attributes = {}
-        self.children = []
-        self._node = node
-    @property
-    def node(self) -> "Node":
-        global node_types
-        if self._node is None:
-            if self.type not in node_types:
-                raise ValueError(f"Invalid node type: {type}")
-            self._node = node_types[self.type](self)
-        return self._node
-    def astype(self, type: NodeType) -> "NodeData":
-        if type == self.type:
-            return self
-        print(f"??? {self}: {self.type} -> {type}")
-        self.type = type
-        self._node = None
+        self.attributes: dict[str, "Attr"] = {}
+        self.children: list["Node"] = []
+    def get_children_with_type(self, type: NodeType):
+        return [child for child in self.children if child.type == type]
+    def get_child_with_type(self, type: NodeType):
+        return self.get_children_with_type(type)[0]
+    def append_child(self, child: "Node"):
+        self.children.append(child)
         return self
-    def __getitem__(self, key):
-        default = None
-        if type(key) == tuple:
-            default_ctor = key[1]
-            key = key[0]
-        if key in self.attributes:
-            return self.attributes[key]
-        if default_ctor is not None:
-            default = default_ctor()
-            self.attributes[key] = default
-            return default
-        return None
-    def __setitem__(self, key: str, value):
-        self.attributes[key] = value
+    def write(self, out, depth):
+        if depth > 3:
+            out.write("...")
+            return
+        indent = "    " * depth
+        out.write(f"{indent}({self.type} (")
+        head = ""
+        for k, a in self.attributes.items():
+            out.write(f"{head}{a.name}={repr(getattr(self, a.private_name))}")
+            head = " "
+        if len(self.children) > 0:
+            out.write(")\n")
+            for child in self.children:
+                child.write(out, depth + 1)
+            out.write(f"{indent})\n")
+        else:
+            out.write("))\n")
     def __str__(self):
-        return f"({self.type} {self.attributes})"
+        out = io.StringIO()
+        self.write(out, 0)
+        return out.getvalue()
 
-class Node:
-    def __init__(self, type: NodeType, data: Optional[NodeData] = None):
-        self.data = data.astype(type) if data is not None else NodeData(type, self)
-    def __str__(self):
-        return str(self.data)
+class NodeAttr:
+    def __init__(self, default_value=None):
+        self.default_value = default_value
+    def __set_name__(self, owner: Node, name: str):
+        self.name = name
+        self.private_name = f"_{name}"
+    def __get__(self, obj: Node, objtype=None):
+        if hasattr(obj, self.private_name):
+            return getattr(obj, self.private_name)
+        return self.default_value
+    def __set__(self, obj: Node, value):
+        obj.attributes[self.name] = self
+        setattr(obj, self.private_name, value)
+
+class NodeChildren:
+    def __init__(self, child_node_type: NodeType):
+        self.child_node_type = child_node_type
+    def __get__(self, obj: Node, objtype=None) -> list[Node]:
+        return obj.get_children_with_type(self.child_node_type)
+    def __set__(self, obj: Node, value: list[Node]):
+        num_children = len(obj.children)
+        i = 0
+        while i < len(obj.children):
+            if obj.children[i].type == self.child_node_type:
+                obj.children.pop(i)
+            else:
+                i += 1
+        for child in value:
+            if child.type != self.child_node_type:
+                raise ValueError(f"Expected child of type {self.child_node_type}, got {child.type}")
+            obj.children.insert(num_children, child)
+            num_children += 1
+
+class NodeChild:
+    def __init__(self, child_node_type: NodeType):
+        self.child_node_type = child_node_type
+    def __get__(self, obj: Node, objtype=None) -> list[Node]:
+        return obj.get_child_with_type(self.child_node_type)
+    def __set__(self, obj: Node, value: Node):
+        if value.type != self.child_node_type:
+            raise ValueError(f"Expected child of type {self.child_node_type}, got {value.type}")
+        num_children = len(obj.children)
+        i = 0
+        while i < len(obj.children):
+            if obj.children[i].type == self.child_node_type:
+                obj.children.pop(i)
+            else:
+                i += 1
+        obj.children.insert(num_children, value)
 
 class Function(Node):
-    def __init__(self, data: Optional[NodeData] = None):
-        super().__init__(NodeType.FUNCTION, data)
+    name = NodeAttr()
+    parameters = NodeChildren(NodeType.PARAMETER)
+    returnType = NodeChild(NodeType.TYPE)
 
-    @property
-    def name(self):
-        return self.data['name', ""]
-    def set_name(self, name: str) -> "Function":
-        self.data['name'] = name
-        return self
-    @property
-    def parameters(self):
-        return self.data['parameters', list]
+    def __init__(self, name: str = None):
+        super().__init__(NodeType.FUNCTION)
+        if name is not None:
+            self.name = name
+
     def parameter(self, name: str, param_type: TypeRef = None) -> "Function":
         if type(name) == tuple:
             name, param_type = name
-        self.parameters.append((name, param_type))
+        p = Parameter(name, param_type)
+        self.append_child(p)
         return self
 
-node_types = {
-    NodeType.FUNCTION: Function,
-}
+class Parameter(Node):
+    name = NodeAttr()
+    parameter_type = NodeChild(NodeType.TYPE)
+
+    def __init__(self, name: str = None, parameter_type: "Type" = None):
+        super().__init__(NodeType.PARAMETER)
+        if name is not None:
+            self.name = name
+        if parameter_type is not None:
+            self.parameter_type = parameter_type
+
+class Type(Node):
+    name = NodeAttr()
+
+    def __init__(self, name: str = None):
+        super().__init__(NodeType.TYPE)
+        if name is not None:
+            self.name = name
