68ba0615145d783fd21ca2450d12c859cd2efed3
Frank A. Krueger <fak@praeclarum.org>
2024-10-10 17:09:40 -0700

    Rename

--- src/alang/languages/a.py
+++ src/alang/languages/a.py
@@ -1,6 +1,6 @@
 from language import Language, register_language
 
-class ALang(Language):
+class ALanguage(Language):
     def __init__(self):
         super().__init__("alang")
         self.set_file_extension(".a")
@@ -8,6 +8,6 @@ class ALang(Language):
     def get_run_command(self, file_path):
         return ["python3", file_path]
 
-a_lang = ALang("alang")
+a_lang = ALanguage("alang")
 register_language(a_lang)
 
