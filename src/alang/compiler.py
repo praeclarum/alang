77d7f48a9d49957c851dc98b8e535cc8f5492fef
Frank A. Krueger <fak@praeclarum.org>
2024-10-12 11:26:54 -0700

    Change visitor dispatch to make val propagation easier

--- src/alang/compiler.py
+++ src/alang/compiler.py
@@ -5,22 +5,24 @@ class DepthFirstVisitor:
         cvals = []
         for crel, child in node.links:
             cvals.append(self.visit(child, node, crel))
-        return self.visit_generic(node, parent, rel, cvals)
-
-    def visit_generic(self, node: Node, parent: Node, rel: str, cvals: list):
         if node.node_type == NodeType.FUNCTION:
             return self.visit_function(node, parent, rel, cvals)
+        else:
+            return self.visit_generic(node, parent, rel, cvals)
+
+    def visit_generic(self, node: Node, parent: Node, rel: str, cvals: list):
         return None
 
     def visit_function(self, node: Node, parent: Node, rel: str, cvals: list):
         return None
     
 class InferFunctionReturnType(DepthFirstVisitor):
+    def visit_generic(self, node: Node, parent: Node, rel: str, cvals: list):
+        return any(cvals)
     def visit_function(self, node: Node, parent: Node, rel: str, cvals: list):
-        print("Visiting function", node.name)
-        if node.return_type is None:
-            return False
-        return True
+        if node.return_type is not None:
+            return any(cvals)
+        raise NotImplementedError("Cannot infer return type of function")
 
 class Compiler:
     def __init__(self, ast: Node):
