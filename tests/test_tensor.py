ca46c519a2ad43060a64c9b9ce7bc40f13280cb7
Frank A. Krueger <fak@praeclarum.org>
2024-10-13 16:07:34 -0700

    Add small test

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -30,7 +30,26 @@ def test_3x5_times_5x7_type():
     t3 = t1 @ t2
     assert t3.shape == (3, 7)
 
-def test_3x5_times_5x7():
+def test_1x2_matmul_2x1():
+    at = tensor_type((1, 2), int_type)
+    bt = tensor_type((2, 1), int_type)
+    ot = at @ bt
+    f = define("f").param("a", at).param("b", bt).ret("a @ b")
+    write_standalone_html("test_1x2_times_2x1", f)
+    code = f.wgsl_code
+    assert code.strip() == """
+fn mul_int1x2_int2x1(a: int1x2, b: int2x1) -> int1x1 {
+for (var out_r: i32 = 0; out_r < 1; ++out_r) {
+for (var out_c: i32 = 0; out_c < 1; ++out_c) {
+o[((out_r * 1) + out_c)] = ((a[(out_r * 2)] * b[out_c]) + (a[((out_r * 2) + 1)] * b[(1 + out_c)]));
+}
+}
+}
+fn f(a: int1x2, b: int2x1) -> int1x1 {
+    return mul_int1x2_int2x1(a, b);
+}""".strip()
+
+def test_3x5_matmul_5x7():
     t1 = tensor_type((3, 5), int_type)
     t2 = tensor_type((5, 7), int_type)
     t3 = t1 @ t2
